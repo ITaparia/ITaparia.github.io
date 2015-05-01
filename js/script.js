b0a98e7453534896dc933ee0b66bcfc377016478
Ishan Taparia <dualstrike99@gmail.com>
2015-05-01 01:29:26 -0500

    First Commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(function(){
+      $(".typing_tester").typed({
+        strings: ["a web developer. ^1000", "a web designer ^1000", "Ishan Taparia"],
+        typeSpeed: 30,
+        backSpeed: 30,
+        showCursor: false
+        
+      });
+  });
+
